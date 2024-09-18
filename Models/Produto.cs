7386ce48f7934d03e80b809e548e3389c82f66d1
PedroWatermann <pedro.programmer.pt@gmail.com>
2024-09-17 22:18:02 -0300

    Início do projeto em sala de aula 17-09-24

---
+++ Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vendas.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public int quantidade { get; set; }
+        public decimal preco { get; set; }
+    }
+}
--- frmProduto.cs
+++ frmProduto.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace vendas
 {
-    public partial class Form1 : Form
+    public partial class frmProduto : Form
     {
-        public Form1()
+        public frmProduto()
         {
             InitializeComponent();
         }
